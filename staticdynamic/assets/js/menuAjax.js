38308090a8cc87c060bed280258aba935c359687
Mohd Ishrat <ishrat@ebrandz.com>
2019-12-24 18:26:08 +0530

    webpage added

--- staticdynamic/assets/js/menuAjax.js
+++ staticdynamic/assets/js/menuAjax.js
@@ -10,3 +10,26 @@ function menuAjax(url)
 		}
 	})
 }
+
+function ajaxPost(url, data)
+{
+	var result;
+	$.ajax(
+	{
+		url:url,
+		type:'post',
+		data:data,
+		dataType:'json',
+		async:false,
+		success:function(response)
+		{
+			result = response;
+		},
+		error:function(response)
+		{
+			result = response;
+		}
+	})
+
+	return result;
+}
