410dbdd859871dcb671dece3dbcd3b730ac22282
Mohd Ishrat <mohdishrat96@gmail.com>
2019-12-28 23:28:35 +0530

    validation added
    validation for forms added

--- youtubetomp3/assets/js/ajaxRequest/signup.js
+++ youtubetomp3/assets/js/ajaxRequest/signup.js
@@ -1,9 +1,19 @@
+var i = 0;
+var txt;
+var speed = 50;
 $('#signup1').click(function()
 {
+	i = 0;
+	txt = '';
 	var name = $('#name').val();
 	var email = $('#email').val();
 	var password = $('#password').val();
 	var rpassword = $('#rpassword').val();
+	$('#errorSignup').html('');
+	if (validateFieldssignup(name, email, password, rpassword) == false)
+	{
+		return false;
+	}
 	var data = {name:name, email:email,password:password, requestFrom:'staticdynamic'};
 	var url = 'auth/register';
 	var result = postRequest(url, data);
@@ -19,3 +29,55 @@ $('#signup1').click(function()
 		alert('error');
 	}
 })
+
+function validateFieldssignup(name, email, password, rpassword)
+{
+	var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+	if (name == '')
+	{
+		txt = 'Empty Name';
+		typeWritersignup();
+		return false;
+	}
+	else if (name.length < 3)
+	{
+		txt = 'Too short Name';
+		typeWritersignup();
+		return false;
+	}
+	else if (!pattern.test(email))
+	{
+		txt = 'Not a valid Email';
+		typeWritersignup();
+		return false;
+	}
+
+	if (password == '')
+	{
+		txt = 'Empty Password';
+		typeWritersignup();
+		return false;
+	}
+	else if (password.length < 6)
+	{
+		txt = 'Too short Password';
+		typeWritersignup();
+		return false;
+	}
+	else if (password != rpassword)
+	{
+		txt = 'Password mismatch!';
+		typeWritersignup();
+		return false;
+	}
+}
+
+function typeWritersignup()
+{
+  	if (i < txt.length)
+	{
+		document.getElementById("errorSignup").innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWritersignup, speed);
+  	}
+}
