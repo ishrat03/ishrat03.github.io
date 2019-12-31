83768afb9b5fb0e46317ea1466f9a6dfb52c4e08
Mohd Ishrat <mohdishrat96@gmail.com>
2019-12-31 22:49:46 +0530

    enter button submit added
    enter button submit added and dashboard load on ajax request

--- youtubetomp3/assets/js/ajaxRequest/getMenu.js
+++ youtubetomp3/assets/js/ajaxRequest/getMenu.js
@@ -1,6 +1,16 @@
+$(document).ready(function()
+{
+	getMenu('playlist.html', 'dashboardloader');
+})
 $('#userProfile').click(function(e)
 {
 	e.preventDefault();
 	getMenu('userProfile.html', 'userloader');
 	$.getScript('../assets/js/ajaxRequest/editUserProfile.js');
+})
+
+$('#dashboardMenu').click(function(e)
+{
+	e.preventDefault();
+	getMenu('playlist.html', 'dashboardloader');
 })
